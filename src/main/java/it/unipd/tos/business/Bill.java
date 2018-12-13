eb28e0b5e1cce9d0a5399896242d4f15ee639ef0
Lin <zhaohui.lin96@gmail.com>
2018-12-13 10:29:13 +0100

    close #2

--- src/main/java/it/unipd/tos/business/Bill.java
+++ src/main/java/it/unipd/tos/business/Bill.java
@@ -14,12 +14,22 @@ public class Bill implements RestaurantBill{
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException{
         double total=0;
 
-        for (MenuItem m : itemsOrdered) {
+        long pizze = itemsOrdered.stream().filter(a -> a.getType().equals(MenuItem.itemType.PIZZA)).count();
+        if(pizze>10){
+            double min = itemsOrdered.get(0).getPrice();
+            for (MenuItem m : itemsOrdered) {
+                total += m.getPrice();
+                if(m.getType().equals(MenuItem.itemType.PIZZA) && m.getPrice()<min){
+                    min=m.getPrice();
+                }
+            }
+            total -= min;
+        }
+        else {
+            for (MenuItem m : itemsOrdered) {
                 total += m.getPrice();
             }
         }
-
-      
         return total;
     }
 }
