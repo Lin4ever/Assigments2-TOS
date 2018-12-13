717fb2f32461bcde0498c4e7c3d5043e46f8cedc
Lin <zhaohui.lin96@gmail.com>
2018-12-13 10:37:16 +0100

    Test 2,#5 in progress

--- src/test/java/it/unipd/tos/BillTest.java
+++ src/test/java/it/unipd/tos/BillTest.java
@@ -33,5 +33,29 @@ public class BillTest {
             System.out.println(e.getMessage());
         }
     }
+    @org.junit.Test
+    public void testOrdinePiuDi10Elementi(){
+        try {
+            List<MenuItem> ordini = new ArrayList<MenuItem>();
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "frutti di mare", 11.9));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "risotto al tartufo", 12.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "marinara", 3));
+			ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+			ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "margherita", 3.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            Bill b = new Bill();
+            double tot = b.getOrderPrice(ordini);
+            assertEquals(90.4, tot, 0.0);
+        }
+        catch(RestaurantBillException e){
+            System.out.println(e.getMessage());
+        }
+    }
    
 }
