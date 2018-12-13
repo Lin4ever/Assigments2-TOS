2d7f21043301e643baff7f145cdb0ade3f9a4a31
Lin <zhaohui.lin96@gmail.com>
2018-12-13 10:40:28 +0100

    Test 4,close #5

--- src/test/java/it/unipd/tos/BillTest.java
+++ src/test/java/it/unipd/tos/BillTest.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class BillTest {
 
-   @org.junit.Test
+      @org.junit.Test
     public void testOrdineNormale(){
         try {
             List<MenuItem> ordini = new ArrayList<MenuItem>();
@@ -87,4 +87,45 @@ public class BillTest {
     }
 
    
+
+    @org.junit.Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @org.junit.Test
+    public void testOrdineConPiuDi20Elementi(){
+        try {
+            List<MenuItem> ordini = new ArrayList<MenuItem>();
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "frutti di mare", 11.9));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "risotto al tartufo", 12.));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "margherita", 3.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "marinara", 3));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "spaghetti aglio olio e peperoncino", 11.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "spaghetti aglio olio e peperoncino", 11.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+			ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "marinara", 3));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+			ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+			ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "marinara", 3));
+            Bill b = new Bill();
+            double tot = b.getOrderPrice(ordini);
+
+            exception.expect(RestaurantBillException.class);
+            exception.expectMessage("Errore! ci sono più di 20 elementi nell'ordine");
+        }
+        catch(RestaurantBillException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
