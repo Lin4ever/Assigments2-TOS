b0ad752df1e2798d7849d07eacc27262d53fb027
Lin <zhaohui.lin96@gmail.com>
2018-12-13 10:37:43 +0100

    Test 3,#5 in progress

--- src/test/java/it/unipd/tos/BillTest.java
+++ src/test/java/it/unipd/tos/BillTest.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class BillTest {
 
-    @org.junit.Test
+   @org.junit.Test
     public void testOrdineNormale(){
         try {
             List<MenuItem> ordini = new ArrayList<MenuItem>();
@@ -57,5 +57,34 @@ public class BillTest {
             System.out.println(e.getMessage());
         }
     }
+   
+    @org.junit.Test
+    public void testScontoPiu100Euro(){
+        try {
+            List<MenuItem> ordini = new ArrayList<MenuItem>();
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "frutti di mare", 11.9));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "risotto al tartufo", 12.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "margherita", 3.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "capricciosa", 8.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "montanara", 9.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "marinara", 3));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PIZZA, "tonno e cipolla", 4.5));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "lasagne alla bolognese", 13.0));
+            ordini.add(new MenuItem(MenuItem.itemType.PRIMO, "spaghetti aglio olio e peperoncino", 11.0));
+            Bill b = new Bill();
+            double tot = b.getOrderPrice(ordini);
+            assertEquals(127.205, tot, 0.0);
+        }
+        catch(RestaurantBillException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
    
 }
